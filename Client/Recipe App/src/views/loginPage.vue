bbb09ccf4aa1e13037a9547065976d20a9c6bb84
Alistair Campbell <AJCampbell@conestogac.on.ca>
2023-10-24 02:20:29 -0400

    added animations to login page

--- Client/Recipe App/src/views/loginPage.vue
+++ Client/Recipe App/src/views/loginPage.vue
@@ -3,8 +3,8 @@
         <!-- image -->
 
         <img src="../assets/loginPage.png"
-            class="absolute max-w-[50%] h-screen object-cover transition-all ease-out duration-700"
-            :class="{ 'left-1/2': signup }">
+            class="absolute max-w-[50%] h-screen object-cover ease-in-out duration-300 drop-shadow-lg"
+            :class="{ 'transition transform translate-x-full': signup }">
         <!-- sign up page -->
         <div class="flex flex-col justify-center mx-auto w-[375px]">
             <h1 class="text-[40px] font-semibold text-main-font mb-10">Sign Up</h1>
@@ -16,7 +16,7 @@
                 <label for="" class="mb-1 text-xl text-main-font">Password</label>
                 <input type="password" placeholder="Enter your password"
                     class="border border-grey-near rounded-md focus:outline-none py-1 px-2 mb-14">
-                <input type="submit" value="Log in"
+                <input type="submit" value="Sign Up"
                     class="bg-green-dark rounded-md h-12 text-2xl font-bold text-green-extra-dark">
             </form>
             <p class="text-center text-main-font mt-7 hover:underline hover:cursor-pointer" @click="signup = false">Back to
