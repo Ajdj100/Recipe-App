0ee0b56b04fcf80a9fd38bceaab9568847e26db7
Alistair Campbell <AJCampbell@conestogac.on.ca>
2023-10-24 03:54:24 -0400

    did stuff

--- Client/Recipe App/src/views/loginPage.vue
+++ Client/Recipe App/src/views/loginPage.vue
@@ -8,16 +8,16 @@
         <!-- sign up page -->
         <div class="flex flex-col justify-center mx-auto w-[375px]">
             <h1 class="text-[40px] font-semibold text-main-font mb-10">Sign Up</h1>
-            <!-- login box -->
-            <form class="flex flex-col" @submit.prevent="">
+            <!-- signup box -->
+            <form class="flex flex-col" @submit.prevent="signupMethod">
                 <label for="" class="mb-1 text-xl text-main-font">Username</label>
-                <input type="text" placeholder="Enter your username"
+                <input type="text" v-model="SignUpUsername" placeholder="Enter your username"
                     class="border border-grey-near rounded-md focus:outline-none py-1 px-2 mb-5">
                 <label for="" class="mb-1 text-xl text-main-font">Password</label>
-                <input type="password" placeholder="Enter your password"
+                <input type="password" v-model="SignUpPassword" placeholder="Enter your password"
                     class="border border-grey-near rounded-md focus:outline-none py-1 px-2 mb-14">
-                <input type="submit" value="Sign Up"
-                    class="bg-green-dark rounded-md h-12 text-2xl font-bold text-green-extra-dark">
+                    <input type="submit" value="Sign Up"
+                    class="bg-green-dark rounded-md h-12 text-2xl font-bold text-green-extra-dark hover:cursor-pointer">
             </form>
             <p class="text-center text-main-font mt-7 hover:underline hover:cursor-pointer" @click="signup = false">Back to
                 Login</p>
@@ -26,16 +26,16 @@
         <div class="flex flex-col justify-center mx-auto w-[375px]">
             <h1 class="text-[40px] font-semibold text-main-font mb-10">Login</h1>
             <!-- login box -->
-            <form class="flex flex-col" @submit.prevent="">
+            <form class="flex flex-col" @submit.prevent="login">
                 <label for="" class="mb-1 text-xl text-main-font">Username</label>
-                <input type="text" placeholder="Enter your username"
+                <input type="text" v-model="LogInUsername" placeholder="Enter your username"
                     class="border border-grey-near rounded-md focus:outline-none py-1 px-2 mb-5">
                 <label for="" class="mb-1 text-xl text-main-font">Password</label>
-                <input type="password" placeholder="Enter your password"
+                <input type="password" v-model="LogInPassword" placeholder="Enter your password"
                     class="border border-grey-near rounded-md focus:outline-none py-1 px-2">
                 <p class="text-right hover:cursor-pointer hover:underline mb-9">Forgot password?</p>
                 <input type="submit" value="Log in"
-                    class="bg-green-dark rounded-md h-12 text-2xl font-bold text-green-extra-dark">
+                    class="bg-green-dark rounded-md h-12 text-2xl font-bold text-green-extra-dark hover:cursor-pointer">
             </form>
             <p class="text-center text-main-font mt-7 hover:underline hover:cursor-pointer" @click="signup = true">No
                 account? Sign up</p>
@@ -48,19 +48,57 @@ export default {
     data() {
         return {
             signup: false,
-            username: '',
-            password: '',
+            SignUpUsername: '',
+            SignUpPassword: '', 
+            LogInUsername: '',
+            LogInPassword: '',
         }
     },
     methods: {
+        signupMethod() {
+            console.log(this.SignUpUsername + this.SignUpPassword)
+            //todo: make API call
+            const request = {
+                method: 'post',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ Username: this.SignUpUsername, Password: this.SignUpPassword })
+            };
+            fetch('http://localhost:8080/register', request)
+                .then(response => {
+                    if (response.status === 200) {
+                        response.json().then((data) => {
+                            localStorage.setItem("user", data.token);
+                        });
+                        this.$router.push('/')
+                    } else {
+                        this.invalid = true;
+                        console.log("bad");
+                    }
+                    //else show invalid login message
+                })
+        },
         login() {
             //todo: make API call
-            console.log("login");
-
-        },
-        signup() {
-            
+            const request = {
+                method: 'post',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ Username: this.LogInUsername, Password: this.LogInPassword })
+            };
+            fetch('http://localhost:8080/login', request)
+                .then(response => {
+                    if (response.status === 200) {
+                        response.json().then((data) => {
+                            localStorage.setItem("user", data.token);
+                        });
+                        this.$router.push('/')
+                    } else {
+                        this.invalid = true;
+                        console.log("bad");
+                    }
+                    //else show invalid login message
+                })
         }
+
     }
 }
 </script>
---
+++ Server/index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const mysql = require('mysql2')
+const cors = require('cors');
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Thataj100!',
+    database: 'foodapp'
+})
+con.connect()
+const app = express()
+const port = 8080
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.post('/login', (req, res) => {
+
+    var sql = `SELECT * FROM users WHERE username='${req.body.Username}' AND password='${req.body.Password}'`
+    con.query(sql, function (err, result) {
+        if (err) throw (err)
+
+        if (result.length == 1) {
+            console.log("good");
+            res.status(200);
+        } else {
+            console.log("bad");
+            res.status(401);
+        }
+
+    })
+    res.status();
+})
+
+app.post('/register', (req, res) => {
+    console.log("create account");
+    console.log("BODY " + req.body);
+    var sql = `INSERT INTO users (username, password) 
+    VALUES ('${req.body.Username}', '${req.body.Password}');`
+    con.query(sql, function (err, result) {
+        if (err) throw (err)
+        console.log("user created");
+        res.status(201);
+    });
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
