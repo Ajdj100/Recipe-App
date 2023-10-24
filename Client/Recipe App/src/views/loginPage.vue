ebc96aa93b7f0fa5bf9574acebb7202b30bbf3e6
Alistair Campbell <AJCampbell@conestogac.on.ca>
2023-10-24 02:14:15 -0400

    added almost all of login page (without animations)

---
+++ Client/Recipe App/src/views/loginPage.vue
@@ -0,0 +1,63 @@
+<template>
+    <div id="parent" class="flex flex-row bg-green-light h-screen">
+        <!-- image -->
+
+        <img src="../assets/loginPage.png"
+            class="absolute max-w-[50%] h-screen object-cover transition-all ease-out duration-700"
+            :class="{ 'left-1/2': signup }">
+        <!-- sign up page -->
+        <div class="flex flex-col justify-center mx-auto w-[375px]">
+            <h1 class="text-[40px] font-semibold text-main-font mb-10">Sign Up</h1>
+            <!-- login box -->
+            <form class="flex flex-col" @submit.prevent="">
+                <label for="" class="mb-1 text-xl text-main-font">Username</label>
+                <input type="text" placeholder="Enter your username"
+                    class="border border-grey-near rounded-md focus:outline-none py-1 px-2 mb-5">
+                <label for="" class="mb-1 text-xl text-main-font">Password</label>
+                <input type="password" placeholder="Enter your password"
+                    class="border border-grey-near rounded-md focus:outline-none py-1 px-2 mb-14">
+                <input type="submit" value="Log in"
+                    class="bg-green-dark rounded-md h-12 text-2xl font-bold text-green-extra-dark">
+            </form>
+            <p class="text-center text-main-font mt-7 hover:underline hover:cursor-pointer" @click="signup = false">Back to
+                Login</p>
+        </div>
+        <!-- login page -->
+        <div class="flex flex-col justify-center mx-auto w-[375px]">
+            <h1 class="text-[40px] font-semibold text-main-font mb-10">Login</h1>
+            <!-- login box -->
+            <form class="flex flex-col" @submit.prevent="">
+                <label for="" class="mb-1 text-xl text-main-font">Username</label>
+                <input type="text" placeholder="Enter your username"
+                    class="border border-grey-near rounded-md focus:outline-none py-1 px-2 mb-5">
+                <label for="" class="mb-1 text-xl text-main-font">Password</label>
+                <input type="password" placeholder="Enter your password"
+                    class="border border-grey-near rounded-md focus:outline-none py-1 px-2">
+                <p class="text-right hover:cursor-pointer hover:underline mb-9">Forgot password?</p>
+                <input type="submit" value="Log in"
+                    class="bg-green-dark rounded-md h-12 text-2xl font-bold text-green-extra-dark">
+            </form>
+            <p class="text-center text-main-font mt-7 hover:underline hover:cursor-pointer" @click="signup = true">No
+                account? Sign up</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            signup: false,
+            username: '',
+            password: '',
+        }
+    },
+    methods: {
+        login() {
+            //todo: make API call
+            console.log("login");
+
+        }
+    }
+}
+</script>
--- Client/Recipe App/tailwind.config.js
+++ Client/Recipe App/tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'green-light': '#F4FFF4;',
+        'green-mid': '#BCECD5;',
+        'green-dark': '#85BAA1;',
+        'main-font': '#00274A;',
+        'grey-near': '#DADADA;',
+      },
+      width: {
+        '1/2': '1/2',
+      }
+    },
   },
   plugins: [],
 }
