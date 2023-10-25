bfce212e35dba39b11e280df847c9c20fef0c900
Alistair Campbell <AJCampbell@conestogac.on.ca>
2023-10-24 21:18:40 -0400

    added navbar

---
+++ Client/Recipe App/src/components/navbar.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="w-screen h-20 drop-shadow-lg bg-inherit flex items-center justify-between px-[126px]">
+        <p class="mr-14">logo</p>
+        <div class="flex justify-around w-full">
+            <a>Trending</a>
+            <a>Recipes</a>
+            <a>Meals</a>
+            <a>Cuisines</a>
+            <a>Dietary</a>
+            <a>About Us</a>
+        </div>
+        <!-- search bar -->
+        <div class="w-full">
+            <input type="text" v-model="searchText" placeholder="What are you looking for?"
+                class="border border-grey-near rounded-md focus:outline-none py-1 px-2 mb-5 w-72">
+        </div>
+        <!-- profile -->
+        <div>
+            <div class="h-12 w-12 bg-green-dark rounded-full" @click="profileDropdown = true">
+
+            </div>
+            <!-- profile options -->
+            <div v-show="profileDropdown">
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            searchText: '',
+            profileDropdown: false,
+
+        }
+    }
+}
+
+</script>
--- Client/Recipe App/src/views/mainPage.vue
+++ Client/Recipe App/src/views/mainPage.vue
@@ -1,4 +1,19 @@
 <template>
-
-    MAIN PAGE
+    <div class="bg-green-light h-screen overflow-clip">
+        <navbar></navbar>
+        <!-- content -->
+        <div class="h-full bg-inherit">
+            <RouterView />
+        </div>
+    </div>
 </template>
+
+<script>
+import navbar from '../components/navbar.vue';
+
+export default {
+    components: {
+        navbar
+    }
+}
+</script>
